7b891c4f47593ab1dd253c3be615d7cf1ebf1a10
Kartik Ramkrishnan <ramkr004@umn.edu>
2015-06-13 16:59:08 -0500

    simple_example added

---
+++ build/opt/version.h
@@ -0,0 +1,2 @@
+#define ZSIM_BUILDDATE "Sat Jun 13 16:54:57 CDT 2015"
+#define ZSIM_BUILDVERSION "master:64:188d0c3:clean"
---
+++ simple_prog.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+      int j=0;
+      for(int i=0;i<1000000000;i++){
+          j++;
+      }
+}
